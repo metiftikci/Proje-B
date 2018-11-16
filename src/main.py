d1e53026c842faa3ed1fd7b212213322f562763d
Muhammed Tiftikçi <muhammed@rst.com.tr>
2018-11-16 13:18:48 +0300

    Added sms api settings.

--- src/main.py
+++ src/main.py
@@ -16,5 +16,5 @@ if True:
         person_email_address = person[settings.DATABASE_INDEX_EMAIL_ADDRESS]
         person_phone_no = person[settings.DATABASE_INDEX_PHONE_NO]
 
-        utils.send_mail(settings.STMP_HOST, settings.STMP_HOST, settings.EMAIL_ADDRESS, settings.EMAIL_PASSWORD, person_email_address, subject, body)
-        # utils.send_sms_mutlucell()
+        utils.send_mail(settings.EMAIL_API_STMP_HOST, settings.EMAIL_API_STMP_HOST, settings.EMAIL_API_EMAIL_ADDRESS, settings.EMAIL_API_EMAIL_PASSWORD, person_email_address, subject, body)
+        utils.send_sms_mutlucell(settings.SMS_API_USERNAME, settings.SMS_API_PASSWORD, settings.SMS_API_ORIGINATOR, body, [person_phone_no])
